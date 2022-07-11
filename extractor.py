66b084c9c026abf36702dfebbe191b2abadf2b47
ADISH.A.P <helloadish007@gmail.com>
2022-07-11 21:16:00 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -14,12 +14,7 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 from webdriver_manager.chrome import ChromeDriverManager
 import os, sys
 
-@st.experimental_singleton
-#def installff():
-#  os.system('sbase install geckodriver')
-#  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
-#_ = installff()
 
 
 
